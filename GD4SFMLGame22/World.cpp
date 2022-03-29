f222a0b17aea0438ef8af9e7edcb2fa0fa26b86f
Neil Ogbeide <d00205601@student.dkit.ie>
2022-03-29 16:47:17 +0100

    Stopped balls from scrolling

--- GD4SFMLGame22/World.cpp
+++ GD4SFMLGame22/World.cpp
@@ -138,7 +138,7 @@ void World::CreatePickup(sf::Vector2f position, PickupType type)
 {
 	std::unique_ptr<Pickup> pickup(new Pickup(type, m_textures));
 	pickup->setPosition(position);
-	pickup->SetVelocity(0.f, 1.f);
+	pickup->SetVelocity(0.f, 0.f);
 	m_scene_layers[static_cast<int>(Layers::kUpperAir)]->AttachChild(std::move(pickup));
 }
 
